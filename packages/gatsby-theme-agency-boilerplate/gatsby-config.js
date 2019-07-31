f35c0b26652a7fe4fce558d971c8c8e76c5cdc30
Michael Crook <ausiemick@gmail.com>
2019-07-31 19:49:05 +0800

    added yaml support

--- packages/gatsby-theme-agency-boilerplate/gatsby-config.js
+++ packages/gatsby-theme-agency-boilerplate/gatsby-config.js
@@ -1,9 +1,20 @@
-module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-  ],
+module.exports = options => {
+  const { contentPath = `src/pages` } = options;
+
+  return {
+    siteMetadata: {
+      title: `Gatsby Default Starter`,
+      description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+      author: `@gatsbyjs`,
+    },
+    plugins: [
+      `gatsby-transformer-yaml`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: contentPath,
+        },
+      },
+    ],
+  }
 }
