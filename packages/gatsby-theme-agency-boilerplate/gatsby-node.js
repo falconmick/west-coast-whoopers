aed5ab185846149916fe651e81ee92c6ee68a497
Michael Crook <ausiemick@gmail.com>
2019-07-31 19:18:56 +0800

    removed a bunch more boilerplate and got file bootstrap working

--- packages/gatsby-theme-agency-boilerplate/gatsby-node.js
+++ packages/gatsby-theme-agency-boilerplate/gatsby-node.js
@@ -1,7 +1,31 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const Debug = require(`debug`)
+const fs = require(`fs`)
+const path = require(`path`)
+const mkdirp = require(`mkdirp`)
 
-// You can delete this file if you're not using it
+const debug = Debug(`gatsby-theme-mdx-atom`)
+
+// These are customizable theme options we only need to check once
+let basePath
+let contentPath
+
+// Ensure that content directories exist at site-level
+exports.onPreBootstrap = ({ store }, themeOptions) => {
+    debug(`Step onPreBootstrap`)
+    const { program } = store.getState()
+
+    basePath = themeOptions.basePath || `/`
+    contentPath = themeOptions.contentPath || `src/pages`
+
+    const dirs = [
+        path.join(program.directory, contentPath),
+    ]
+
+    dirs.forEach(dir => {
+        debug(`Initializing ${dir} directory`)
+        if (!fs.existsSync(dir)) {
+            mkdirp.sync(dir)
+        }
+    })
+    debug(`Step onPreBootstrap end`)
+}
